7966c6affefcb622d70d13b3674fdd6384c6cc45
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-10 12:17:27 +0530

    Add files via upload

---
+++ countchar.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class countchar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string");
+        String s=sc.nextLine();
+        int vowels=0;int cons=0;int specialchar=0;int num=0;
+        s=s.toLowerCase();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' ||ch=='u')
+            {
+                vowels++;
+            }
+            else if(ch>='a' && ch<='z')
+            {
+                cons++;
+            }
+            else if(ch>='0' && ch<='9')
+            {
+                num++;
+
+            }
+            else if(!Character.isWhitespace(ch));
+            {
+                specialchar++;
+            }
+        }
+        System.out.println("vowels:"+vowels);
+        System.out.println("consonents:"+cons);
+        System.out.println("numbers"+num);
+        System.out.println("special character:"+specialchar);
+
+    }
+}
